eb9e7bc572da7e5713b6cb9e05701f385d75be05
HiDeo <hideo.sup@gmail.com>
2021-05-27 15:11:46 +0200

    Update homebrew installation

--- setup/homebrew.sh
+++ setup/homebrew.sh
@@ -6,7 +6,7 @@ then
   then
     echo "Installing Homebrew."
 
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   else
     echo "Invalid OS."
     exit 1
