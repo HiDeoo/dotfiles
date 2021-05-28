4ebf36b6b88e6d7367ea2b84a587c984266356e9
HiDeo <hideo.sup@gmail.com>
2021-05-28 17:17:27 +0200

    Clean settings setup script

--- setup/settings.sh
+++ setup/settings.sh
@@ -15,9 +15,6 @@ echo "Setting preferences."
 # General
 #
 
-# Disable the sound effects on boot.
-sudo nvram SystemAudioVolume=" "
-
 # Expand Save Panel by default.
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
@@ -31,9 +28,6 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # Automatically quit the printer app once the print jobs complete.
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-# Disable the “Are you sure you want to open this application?” dialog.
-defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
@@ -59,34 +53,19 @@ defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
 # UI
 #
 
-# Disable the Menu Bar Items for Time Machine, Bluetooth, Airport, Battery & Users.
+# Disable the Menu Bar Items for Bluetooth, Time Machine & Airport.
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Bluetooth -int 18
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
 	defaults write "${domain}" dontAutoLoad -array \
 		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-  	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
-		"/System/Library/CoreServices/Menu Extras/User.menu"
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu"
 done
-
-# Enable the Menu Bar Item for Volume.
-defaults write com.apple.systemuiserver menuExtras -array \
-	"/System/Library/CoreServices/Menu Extras/Volume.menu"
 
 # Set sidebar icon size to small.
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
 # Enable snap-to-grid for desktop icons.
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-#
-# Trackpad
-#
-
-# Enable tap to click
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 #
 # Finder
@@ -132,9 +111,6 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Disable the warning before emptying the Trash.
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Enable AirDrop over Ethernet and on unsupported Macs running Lion.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
@@ -142,8 +118,8 @@ chflags nohidden ~/Library
 # Dock
 #
 
-# Set the icon size of Dock items to 36 pixels.
-defaults write com.apple.dock tilesize -int 50
+# Set the icon size of Dock items.
+defaults write com.apple.dock tilesize -int 40
 
 # Disable size modification.
 defaults write com.apple.dock size-immutable -bool true
@@ -170,33 +146,17 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 # Safari
 #
 
-# Privacy: don’t send search queries to Apple.
-defaults write com.apple.Safari UniversalSearchEnabled -bool false
-defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-
 # Show the full URL in the address bar.
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
-# Set Safari’s home page to `about:blank`.
-defaults write com.apple.Safari HomePage -string "about:blank"
+# Set Safari’s home page to an empty page.
+defaults write com.apple.Safari HomePage -string ""
 
 # Prevent Safari from opening ‘safe’ files automatically after downloading.
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
-# Allow hitting the Backspace key to go to the previous page in history.
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
-
 # Hide Safari’s bookmarks bar by default.
-defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-# Disable Safari’s thumbnail cache for History and Top Sites.
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
-
-# Enable Safari’s debug menu.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Make Safari’s search banners default to Contains instead of Starts With.
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+defaults write com.apple.Safari ShowFavorites -bool false
 
 # Enable the Develop menu and the Web Inspector in Safari.
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
@@ -213,14 +173,10 @@ defaults write com.apple.Safari AutoFillCreditCardData -bool false
 defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
 
 # Block pop-up windows.
-defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
+defaults write com.apple.Safari WebKitPreferences.javaScriptCanOpenWindowsAutomatically -bool false
 
 # Enable “Do Not Track”.
 defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
-
-# Update extensions automatically.
-defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 
 #
 # Activity Monitor
@@ -230,11 +186,31 @@ defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
 # Show all processes in Activity Monitor.
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
+defaults write com.apple.ActivityMonitor ShowCategory -int 100
 
-# Sort Activity Monitor results by CPU usage.
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
+# Set columns for each tab in Activity Monitor.
+defaults write com.apple.ActivityMonitor "UserColumnsPerTab v6.0" -dict \
+	'0' '( Command, CPUUsage, CPUTime, Threads, IdleWakeUps, GPUUsage, PID, UID )' \
+	'1' '( Command, anonymousMemory, compressedMemory, ResidentSize, Threads, Ports, PID, UID )' \
+	'2' '( Command, PowerScore, 12HRPower, AppSleep, graphicCard, powerAssertion, UID )' \
+	'3' '( Command, bytesWritten, bytesRead, PID, UID )' \
+	'4' '( Command, txBytes, rxBytes, txPackets, rxPackets, PID, UID )' \
+	'5' '( Name, LastHour, LastDay, LastWeek, LastMonth )' \
+	'6' '( Command, GPUUsage, GPUTime, PID, UID )'
+
+# Sort columns for each tab in Activity Monitor.
+defaults write com.apple.ActivityMonitor UserColumnSortPerTab -dict \
+	'0' '{ direction = 0; sort = CPUUsage; }' \
+	'1' '{ direction = 0; sort = anonymousMemory; }' \
+	'2' '{ direction = 0; sort = 12HRPower; }' \
+	'3' '{ direction = 0; sort = bytesWritten; }' \
+	'4' '{ direction = 0; sort = txBytes; }'
+
+# Show data graph for the Disk tab in Activity Monitor.
+defaults write com.apple.ActivityMonitor DiskGraphType -int 1
+
+# Show data graph for the Network tab in Activity Monitor.
+defaults write com.apple.ActivityMonitor NetworkGraphType -int 1
 
 #
 # Mac App Store
@@ -253,17 +229,11 @@ defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
 # Misc
 #
 
-# Make ⌘ + F focus the search input in iTunes.
-defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
-
 # Disable smart quotes in Messages as it’s annoying for messages that contain code.
 defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
 
 # Prevent Photos from opening automatically when devices are plugged in.
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-
-# Auto-play videos when opened with QuickTime Player.
-defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use.
 defaults write com.apple.dock mru-spaces -bool false
@@ -276,16 +246,10 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 # Enable zoom using Ctrl + scroll.
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
 
 #
 # END
 #
-
-for app in "Activity Monitor" "cfprefsd" "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Messages" \
-	"Photos" "Safari" "SystemUIServer" "Terminal"; do
-	killall "${app}" &> /dev/null
-done
 
 echo "Done settings preferences. Note that some of these changes require a logout/restart to take effect."
 
